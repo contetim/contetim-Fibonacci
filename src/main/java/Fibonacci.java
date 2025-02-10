718ce9fd195f3745484021524af07b5cdf3ade00
Tim C <conte.tim@gmail.com>
2025-02-10 19:16:48 +0000

    lab completed

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,17 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        if (n <= 1) return n;
+      
+        int[] fibonacci = new int[n + 1];
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        return fibonacci[n];
     }
 }
